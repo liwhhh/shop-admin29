7c64c7417755c5aebb185497e9a8d3e631af8dd3
lwhong <li@whong.com>
2019-06-26 18:05:40 +0800

    axios下载/引入优化完成

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,23 @@
 </template>
 
 <script>
+// // 导入第三方的包
+// import axios from 'axios';
+
 export default {
   name: 'app',
-  // components: {
- 
-  // }
+  
+  mounted(){
+    //axios会返回promise对象,对象下面then这个方法  https://api.github.com/users
+  this.$axios({
+      url:"https://api.github.com/users",
+      method:"GET",
+      params:{},
+    }).then(res => {
+     alert(res)
+    })
+  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,13 +6,16 @@ import ElementUI from 'element-ui';
 // 2.element-ui样式
 import 'element-ui/lib/theme-chalk/index.css';
 
+//1.1引入axios
+import axios from 'axios';
 
 //3.element 全局注册组件
 Vue.use(ElementUI);
 
 // 是否是生成环境
 Vue.config.productionTip = false
-
+//1.2把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios=axios;
 new Vue({
   render: h => h(App),
   
