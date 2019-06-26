8d422d41d1b825bded8465efa5dccae979db963a
lwhong <li@whong.com>
2019-06-26 16:44:31 +0800

    vue基本结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+// 是否是生成环境
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //1.  render: function(createElement) {
+  //    return createElement(App)
+  // }
+  //2.  render: (createElement) => {
+  //   return createElement(App)
+  // }
+  //3.  render: (h) => {
+  //   return h(App)
+  // }
+  //4.  render: (h) => h(App)
+  // $mount作用相当于el,根实例挂载到节点
 }).$mount('#app')
