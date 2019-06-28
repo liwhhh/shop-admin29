5bf89f9ecd181a2748bf323cac78989d683c517a
lwhong <li@whong.com>
2019-06-28 15:24:23 +0800

    用户退出

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,8 @@
   <el-row type="flex" justify="space-between" align="middle">
      <i class="el-icon-back" @click="handleClick"></i>
      <div class="user-info">
-       {{username}} {{realname}} <span>退出</span>
+       {{username}} {{realname}} 
+       <span @click="handleLogout">退出</span>
      </div>
   </el-row>
 </template>
@@ -21,7 +22,27 @@ export default {
       handleClick(){
         //updataCollapse  e mi t
         this.$emit("updataCollapse")
-      }
+      },
+
+      // 用户退出
+      handleLogout(){
+        this.$axios({
+          url:"http://localhost:8899/admin/account/logout"
+        }).then(res=>{
+          const {status} =res.data;
+
+          // if(!status){
+            if(status=== 0){
+            //跳转到登录页  (push不是最优的方案,因为有权限返回上一页)
+            // replace也是处理跳转,直接换掉当前的页面
+             this.$router.replace("/login");
+
+            // 清除本地的数据 
+            localStorage.removeItem("username");
+            localStorage.removeItem("realname");
+          }//if
+        })
+      }//退出
     },
     // 组件加载完成之后拿
     mounted(){
