21edb3de36b44126e3b9eb7b9c320d6e6721c7c2
lwhong <li@whong.com>
2019-06-28 17:33:28 +0800

    商品列表布局

--- src/pages/GoodsList.vue
+++ src/pages/GoodsList.vue
@@ -1,15 +1,102 @@
 <template>
-  <div>
-    indexjaoghu
-  </div>
-</template>
+    <div>
+     <!-- 列表顶部 -->
+     <el-row type="flex" justify="space-between" align="middle">
+      <el-col>
+         <el-button>新增</el-button>
+         <el-button>删除</el-button>
+      </el-col>
+        <div  style="margin-top: 15px;">
+          <el-input placeholder="请输入内容"  class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+     </el-row>
+   <!-- 商品列表 -->
+     <el-table
+    :data="tableData"
+    style="width: 100%">
+    <!-- 允许多选 -->
+     <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      label="日期"
+      width="180">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="姓名"
+      width="180">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>姓名: {{ scope.row.name }}</p>
+          <p>住址: {{ scope.row.address }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 
+
+   </div>
+</template>
 <script>
 export default {
-
+      data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    }
 }
 </script>
 
 <style>
-
+    .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
 </style>
